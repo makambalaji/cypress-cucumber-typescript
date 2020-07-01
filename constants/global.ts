3405306b1c8feb97cb9bb2e5a845bf30587992f0
Balaji Makam <bmakam@redhat.com>
2020-07-01 20:24:19 +0530

    update repo

---
+++ constants/global.ts
@@ -0,0 +1,18 @@
+export enum devNavigationMenu {
+  Add = '+Add',
+  Topology = ' Topology',
+  Monitoring = 'Monitoring',
+  Builds = 'Builds',
+  Search = 'Search',
+  Helm = 'Helm',
+  ProjectDetails = 'Project Details',
+  ProjectAccess = 'Project Access',
+  Pipelines = 'Pipelines',
+  ConfigMaps = 'Config Maps',
+  Secrets = 'Secrets',
+}
+
+export enum switchPerspective {
+  Developer = 'Developer Perspective',
+  Administrator = ' Administrator Perspective',
+}
